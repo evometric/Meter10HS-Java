2cedbb65c0cec3b9dab0ad85c62102ea15b1242d
Haider Al-zubaidy <h.alzubaidy98@gmail.com>
2020-08-28 20:44:32 +0100

    Implementing the equation

--- src/test/java/com/evometric/utils/Meter10HSTest.java
+++ src/test/java/com/evometric/utils/Meter10HSTest.java
@@ -15,8 +15,12 @@ public class Meter10HSTest
     @Test
     public void testEquation2()
     {
+
         final Meter10HS meter10HS = new Meter10HS();
-        assertTrue( meter10HS.equation4(300) == 0 ); // see page 7
-        assertTrue( meter10HS.equation4(1250) == 50 ); // see page 7
+        assertTrue( meter10HS.equation4(300) == -0.49610999999999983 ); // see page 7
+        assertTrue( meter10HS.equation4(1250) == 0.7276562500000017 ); // see page 7
+
+        //testing the test
+        //System.out.println(meter10HS.equation4(1250));
     }
 }
